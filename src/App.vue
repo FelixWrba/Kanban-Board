d959be5596f847bfc233f7329026aa270c8c735a
Felix Wrba <felix.wrba@web.de>
2025-05-10 19:19:58 +0200

    Added NavigationBar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,9 @@
+<script setup>
+import Navigation from "./layout/Navigation.vue";
+</script>
+
 <template>
+  <Navigation />
   <h1>Kanban Board</h1>
   <h2>Description</h2>
   <p>This is a simple Kanban Board.</p>
---
+++ src/layout/Navigation.vue
@@ -0,0 +1,90 @@
+<script setup>
+import { ref } from "vue";
+import { HomeIcon, Cog6ToothIcon } from "@heroicons/vue/24/outline";
+
+const isOpen = ref(false);
+</script>
+
+<template>
+    <nav @mouseover="isOpen = true" @mouseleave="isOpen = false" :class="isOpen ? '' : 'hidden'">
+        <div class="links">
+            <a href="#">
+                <HomeIcon /> Home
+            </a>
+            <a href="#">
+                <Cog6ToothIcon />Settings
+            </a>
+        </div>
+        <div class="legal">
+            <a href="/about">About</a> • <a href="/imprint">Imprint</a>
+            <p>&copy;2025 Felix Wrba. All rights reserved.</p>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+nav {
+    background-color: var(--bg-c);
+    position: fixed;
+    transition: all .3s;
+    max-width: 300px;
+    width: 80%;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    box-shadow: 0px 0px 0px 100vw rgba(0, 0, 0, 0.1);
+    border-right: 1px solid var(--acc-c);
+    padding: 16px;
+}
+
+nav.hidden {
+    box-shadow: 0px 0px 0px 100vw rgba(0, 0, 0, 0);
+    transform: translateX(-95%);
+    animation: vanish 0s .3s both;
+}
+
+@keyframes vanish {
+    100% {
+        opacity: 0;
+    }
+}
+
+.links {
+    display: flex;
+    flex-direction: column;
+}
+
+.links a {
+    display: flex;
+    align-items: center;
+    gap: 4px;
+    padding: 6px;
+    border-radius: 4px;
+    transition: all .3s;
+}
+
+.links a:hover {
+    background-color: var(--secondary-c);
+    transform: scale(1.1);
+}
+
+.links svg {
+    height: 1.5em;
+    width: 1.5em;
+}
+
+.legal {
+    color: var(--acc-c);
+    text-align: center;
+    font-size: .8em;
+    margin-top: 16px;
+}
+
+.legal p {
+    margin-top: 8px;
+}
+
+.legal a:hover {
+    text-decoration: underline;
+}
+</style>
